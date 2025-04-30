1b00e393828bfcd02f0fccf8174fb85c3580eb93
Mohammad Mudassir <67314024+mdmudassir0143@users.noreply.github.com>
2025-05-01 00:36:52 +0530

    Create eg.js
    example

---
+++ eg.js
@@ -0,0 +1,15 @@
+// Advanced example
+await algorand.send.assetCreate({
+  sender: 'CREATORADDRESS',
+  total: 100n,
+  decimals: 2,
+  assetName: 'asset',
+  unitName: 'unit',
+  url: 'url',
+  metadataHash: 'metadataHash',
+  defaultFrozen: false,
+  manager: 'MANAGERADDRESS',
+  reserve: 'RESERVEADDRESS',
+  freeze: 'FREEZEADDRESS',
+  clawback: 'CLAWBACKADDRESS'
+});
